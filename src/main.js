f467f3f16c21f07e525894f5a117a0a0f12f9e8f
Marc A. Casellas <Marc@cyber-wizard.com>
2025-10-26 11:37:54 +0300

    Update imports in main.js for Hashium and Tidio

--- src/main.js
+++ src/main.js
@@ -1,1 +1,2 @@
-import * as Hashium from "https://cdn.jsdelivr.net/gh/casellas-maknikar/hashium/src/main.min.js";
+import "https://cdn.jsdelivr.net/gh/casellas-maknikar/hashium/src/main.min.js"; //Hashium
+import "https://code.tidio.co/cfpipxgbpahegwvhb4416m4xqc5qxgqm.js"; //Tidio
